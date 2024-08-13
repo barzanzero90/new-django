2868b79c130713c83776a6abb8c27b18eda5e185
Barzan <99289946+barzanzero90@users.noreply.github.com>
2024-08-13 15:49:58 -0700

    Add files via upload

--- deploy/settings.py
+++ deploy/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-48(2x!v7s7v0cys=*5r1crtv*0@@3amm-^(+(%5*kb2e!tf=!n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1']
+ALLOWED_HOSTS = ['new-django-mu.vercel.app', '.now.sh', '127.0.0.1']
 
 
 # Application definition
